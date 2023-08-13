538a17b4d4349bca3445234c3be0a532cddf9521
Bartosz Mężyk <bartmezyk@gmail.com>
2023-08-13 17:36:54 +0200

    feat(card): add cards constant with interface

---
+++ app/common/constants.ts
@@ -0,0 +1,52 @@
+import { CardInterface } from "./types";
+
+export const CARDS: CardInterface[] = [
+  {
+    id: 0,
+    name: 'helmet',
+  },
+  {
+    id: 1,
+    name: 'helmet',
+  },
+  {
+    id: 2,
+    name: 'potion',
+  },
+  {
+    id: 3,
+    name: 'potion',
+  },
+  {
+    id: 4,
+    name: 'ring',
+  },
+  {
+    id: 5,
+    name: 'ring',
+  },
+  {
+    id: 6,
+    name: 'scroll',
+  },
+  {
+    id: 7,
+    name: 'scroll',
+  },
+  {
+    id: 8,
+    name: 'shield',
+  },
+  {
+    id: 9,
+    name: 'shield',
+  },
+  {
+    id: 10,
+    name: 'sword',
+  },
+  {
+    id: 11,
+    name: 'sword',
+  },
+]
---
+++ app/common/types.ts
@@ -0,0 +1,4 @@
+export interface CardInterface {
+  id: number;
+  name: string;
+}
