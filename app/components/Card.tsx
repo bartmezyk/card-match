595840a5593a54fda9b90a6d498674f6e7cd58e1
Bartosz Mężyk <bartmezyk@gmail.com>
2023-08-17 13:24:27 +0200

    style(card): update Card component formatting

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -4,23 +4,21 @@ interface CardProps {
   handleClick: () => void;
 }
 
-export const Card = ({ isOpen, name, handleClick }: CardProps) => {
-  return (
+export const Card = ({ isOpen, name, handleClick }: CardProps) => (
+  <div
+    className={`cursor-pointer ${isOpen ? "pointer-events-none" : ""}`}
+    onClick={handleClick}
+  >
     <div
-      className={`cursor-pointer ${isOpen ? "pointer-events-none" : ""}`}
-      onClick={handleClick}
+      className={`preserve-3d transition-all duration-300 border-2 border-solid border-white hover:border-fuchsia-800 rounded w-full h-full card-wrapper ${
+        isOpen ? "rotate-y-180 border-zinc-500" : ""
+      }`}
     >
+      <div className="absolute w-full h-full backface-hidden bg-cardCover" />
       <div
-        className={`preserve-3d transition-all duration-300 border-2 border-solid border-white hover:border-fuchsia-800 rounded w-full h-full card-wrapper ${
-          isOpen ? "rotate-y-180 border-zinc-500" : ""
-        }`}
-      >
-        <div className="absolute w-full h-full backface-hidden bg-cardCover" />
-        <div
-          className="absolute w-full h-full backface-hidden rotate-y-180"
-          style={{ backgroundImage: `url("./img/${name}")` }}
-        />
-      </div>
+        className="absolute w-full h-full backface-hidden rotate-y-180"
+        style={{ backgroundImage: `url("./img/${name}")` }}
+      />
     </div>
-  );
-};
+  </div>
+);
